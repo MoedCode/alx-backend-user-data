4fa060b6898d5e1e6120a5e4a740f121480f15e4
MoedCode <mohamed71291@gmail.com>
2024-05-17 02:24:37 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+#!/usr/bin/env python3
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ called filter_datum that returns the log message obfuscated: """
+    pattern = "|".join(map(re.escape, fields))
+    # print(f"Pattern : {pattern}")
+    regex = re.compile(rf'(?:^|{re.escape(separator)})(?:{pattern})=[^;]*')
+
+    return regex.sub(f'{redaction}', message)
+
+
+# Test the function
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+            "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
