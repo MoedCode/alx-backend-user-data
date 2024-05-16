9e4ee31abc4c0c941946e761e265853af2d777ad
MoedCode <mohamed71291@gmail.com>
2024-05-17 02:06:16 +0300

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,8 +9,6 @@ from bcrypt import hashpw
 def hash_password(password: str) -> bytes:
     """
     Returns a encoded hashed password instance from  <class 'bytes'>
-    Args:
-        password (str): password to be hashed
     """
     b = password.encode()
     hashed = hashpw(b, bcrypt.gensalt())
@@ -20,17 +18,12 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Check  the password  validation
-    Args:
-        hashed_password (bytes):
-        encoded hashed password instance from  <class 'bytes'>
-        password (str): password in string
-    Return:
-        bool
     """
     return bcrypt.checkpw(password.encode(), hashed_password)
 
+
 if __name__ == "__main__":
-    PWD  = "myPass"
+    PWD = "myPass"
     hasPWD = hash_password(PWD)
     print(f"hashed password : {hasPWD}")
     print(f"hashed password : {type(hasPWD)}")
