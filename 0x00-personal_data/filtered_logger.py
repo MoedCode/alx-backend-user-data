c773bc51dc0ae6b6af7c1ce16c33efec47d46855
MoedCode <mohamed71291@gmail.com>
2024-05-17 03:02:35 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,14 +12,11 @@ import mysql.connector
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """
-    Return an obfuscated log message
-    """
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ Return an obfuscated log message """
     for field in fields:
-        message = re.sub(field+'=.*?'+separator,
-                         field+'='+redaction+separator, message)
+        message = re.sub(field+'=.*?'+separator, field +
+                         '='+redaction+separator, message)
     return message
 
 
@@ -36,16 +33,10 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        redact the message of LogRecord instance
-        Args:
-        record (logging.LogRecord): LogRecord instance containing message
-        Return:
-            formatted string
-        """
+        """ redact the message of LogRecord instance """
         message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION,
-                                message, self.SEPARATOR)
+        redacted = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
         return redacted
 
 
@@ -67,16 +58,13 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    """
+    """ def get_db() -> mysql.connector.connection.MySQLConnection: """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
+    conn = mysql.connector.connect(
+        user=user, password=passwd, host=host, database=db_name)
     return conn
 
 
