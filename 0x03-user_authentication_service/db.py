e0364c9a207c503008363e848fe796e3a49d812b
MoedCode <mohamed71291@gmail.com>
2024-05-31 01:13:55 +0300

    Fri May 31 01:13:55 EEST 2024

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,15 +35,15 @@ class DB:
         """instantiate new user add it to database
         """
         new_user = User(email=email, hashed_password=hashed_password)
-        # print(f"user after instantiation  {new_user.__dict__}")
+
         self._session.add(new_user)
         self._session.commit()
-        # print(f"user before return  {new_user.__dict__}")
 
         return new_user
 
     def find_user_by(self, **kwargs):
-        """ return first row in users if keyword matches """
+        """ return first row in users if keyword matches
+        """
         if not kwargs:
             raise InvalidRequestError
         columns_list = User.__table__.columns.keys()
