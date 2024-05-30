b01ed2a00fdd4a41449b064a389c83d589960aa7
MoedCode <mohamed71291@gmail.com>
2024-05-30 23:38:03 +0300

    Thu May 30 23:38:03 EEST 2024

--- 0x03-user_authentication_service/0-user_buckup.py
+++ 0x03-user_authentication_service/0-user_buckup.py
@@ -16,6 +16,13 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+    # def __init__(self, email, hashed_password, session_id=None, reset_token=None):
+    #     self.email = email
+    #     self.hashed_password = hashed_password
+    #     if session_id:
+    #         self.session_id = session_id
+    #     if reset_token:
+    #         self.reset_token = reset_token
 
 
 if __name__ == "__main__":
