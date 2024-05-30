671fba5a603bfba1a29a29a8c7fdd8392de628ef
MoedCode <mohamed71291@gmail.com>
2024-05-31 01:20:26 +0300

    Fri May 31 01:20:26 EEST 2024

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,7 +54,7 @@ class DB:
                 raise InvalidRequestError
         user = self.__session.query(User).filter_by(**kwargs).first()
 
-        if not User:
+        if user is None:
             raise NoResultFound
 
         return user
