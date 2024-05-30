4f2da86404c49bf3b9fb5e7257677dd19f0dfc2e
MoedCode <mohamed71291@gmail.com>
2024-05-30 23:33:52 +0300

    Thu May 30 23:33:52 EEST 2024

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+user after instantiation  {
+    '_sa_instance_state': < sqlalchemy.orm.state.InstanceState object at 0x7fd920969340 > ,
+    'email': 'test@test.com',
+    'hashed_password': 'SuperHashedPwd'}
+
+user before return {
+    '_sa_instance_state': < sqlalchemy.orm.state.InstanceState object at 0x7fd920969340 > }
+
+1
+user after instantiation  {
+    '_sa_instance_state': < sqlalchemy.orm.state.InstanceState object at 0x7fd9209432e0 > ,
+    'email': 'test1@test.com',
+    'hashed_password': 'SuperHashedPwd1'}
+
+user before return {
+    '_sa_instance_state': < sqlalchemy.orm.state.InstanceState object at 0x7fd9209432e0 > }
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,13 +3,11 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-from user import Base
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import (Base, User)
 
 # GLOBALS
 # engine = create_engine('postgresql://user:password@localhost/mydatabase')
@@ -42,8 +40,10 @@ class DB:
         """instantiate new user add it to database
         """
         new_user = User(email=email, hashed_password=hashed_password)
+        # print(f"user after instantiation  {new_user.__dict__}")
         self._session.add(new_user)
         self._session.commit()
+        # print(f"user before return  {new_user.__dict__}")
 
         return new_user
 
